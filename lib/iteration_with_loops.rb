06623b6e0c1d27e15944d8bd17135a095ae421d8
Brian Rhodes <brianr4548@gmail.com>
2020-10-17 01:46:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,7 @@
 def join_nested_strings(src)
 
 src1 = src.flatten(1)
-src2 = src1.select {|strings| strings.to_s.start_with?('b','T','q','f','s','R') }
-
-src3 = src2.join(' ')
+src2 = 
 
 src3
 end
