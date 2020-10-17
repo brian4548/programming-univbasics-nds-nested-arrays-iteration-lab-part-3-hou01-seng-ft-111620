4cf62a6cf97c718ad0ab6c191d1aab2b8d464df9
Brian Rhodes <brianr4548@gmail.com>
2020-10-17 01:48:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,6 +2,7 @@ def join_nested_strings(src)
 
 src1 = src.flatten(1)
 src2 = src1 = src.flatten(1)
-src2 = src1.select{ |ele| ele.is_a?(String) }
+src3 = src2.select{ |ele| ele.is_a?(String) }
+src4 = join(' ')
 p src2
 end
